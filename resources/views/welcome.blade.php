47003a9078915231315392e7600a609ddc792e23
devit7 <deviterlingga.e@gmail.com>
2024-03-05 13:45:59 +0700

    update readme

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,13 @@
 @extends('layouts.master')
 
 @section('content')
-    <div class="container text-center mt-5">
+    <div class="container text-center mt-4">
         <h1>Welcome to Project Integratif</h1>
-        <p>This is a project for the Integratif P1 course.</p>
+        <p>This is a project for the Integratif praktikum 1.</p>
+
+        <div class="mt-5 w-25 mx-auto">
+            <img src="http://img-host-by-dev.vercel.app/img/ayangKu/402791915_316126391187425_7251678098540808968_n.jpg"
+                alt="" class="img-fluid">
+        </div>
     </div>
 @endsection
